9ffdb2fc85bf0c94cdd73033882c84ec15fc8d69
maskikuy <kproject305@gmail.com>
2024-06-14 13:52:09 +0700

    menuliskan void inoerder

--- BinarySearchTree_0060/BinarySearchTree_0060.cpp
+++ BinarySearchTree_0060/BinarySearchTree_0060.cpp
@@ -55,3 +55,15 @@ public:
 				currentNode = currentNode->rightchild;
 		}
 	}
+
+	void inorder(Node* ptr) {
+		if (ROOT == NULL) {
+			cout << "tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL) {
+			inorder(ptr->leftchild);
+			cout << ptr->info << "";
+			inorder(ptr->leftchild);
+		}
+	}
